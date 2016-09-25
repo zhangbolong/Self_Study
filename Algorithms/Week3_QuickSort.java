a38d8ab8744720d8ac8dee829f354354252d7654
zhangbolong <boliu9441@gmail.com>
2016-09-25 16:22:39 -0400

    Finish Week 3 Quicksort

--- Algorithms/Week3_QuickSort.java
+++ Algorithms/Week3_QuickSort.java
@@ -25,7 +25,7 @@ private static int partititon(Comparable[] a, int lo, int hi){
             if(i == hi) break;
         }
 
-        /* Find item on the left to swap */
+        /* Find item on the right to swap */
         while(less(a[lo], a[--j])){
             if(j == lo) break;
         }
@@ -35,7 +35,7 @@ private static int partititon(Comparable[] a, int lo, int hi){
         exch(a, i, j);
     }
 
-    /* Exchange a[lo] and a[j] so that a[j] is in place */
+    /* Exchange a[lo] and a[j] so that a[j] (old a[lo])is in place */
     exch(a, lo, j); 
     /* Returned the elemen which is already in place */
     return j;
@@ -77,7 +77,8 @@ public class QuickImprove extends Quick{
     
 }
 
-//Selection ind the k-th larger/ small in array without sort the whole array.
+//Select the k-th larger/ small in array without sort the whole array.
+//Just like SQL SELECT ..... TOP # / LIMIT #.
 /** Quick-Select
     $ Entry a[j] is in place
     $ No larger entry to the left of j
@@ -85,7 +86,7 @@ public class QuickImprove extends Quick{
 
     $ Average running time: N linear.
 */
-public static Comparable select(Comparable[] a, int k){
+public static Comparable selectTop(Comparable[] a, int k){
     Random.shuffle(a);
     int lo = 0, hi = a.length - 1;
     while( hi > lo){
@@ -101,7 +102,66 @@ public static Comparable select(Comparable[] a, int k){
     }
 }
 
-//Duplicate Keys
-/**
+// Duplicate Keys
+/** Duplicate Keys
+    $ Merge sort with duplicate keys: Always between 1/2 NlgN and NlgN compares
 
+    $ Quick sort with duplicate keys: Goes Quadratic unless partitioning stops
+            on equal keys.
+    $ We want to put all items equal to the partitioning item in place
+    
+    $ Partition array into 3 part so that :
+            1. The middle part are all equal partitioning items
+            2. No larger entries to left of lt
+            3. No smaller entries to right of gt
+        [lo] [.........] [lt] [==========] [gt] [...........] [hi] 
+
+    $ Randomized quicksort with 3 way partitioning reduces running time
+            from NlgN -->  N in most occations.
+
+*/
+
+/** Dutch national flag problem [Dijistra] s-way Partitioning
+
+       $ Let v be partitioning item a[lo]
+       $ Scan i from left to right:
+                1. (a[i] < v): exchange a[lt] with a[i]; i++; li++;
+                2. (a[i] > v): exchange a[gt] with a[i], gt--;
+                3. (a[i] == v): i++;
+      [lo]  [... < V ] [lt] [V] [i] [====v====] [gt] [ > V .....] [hi]
+
+ */
+
+ private static void sort(Comparable[] a, int lo, int hi){
+     if(hi <= lo) return;
+     int lt = lo, gt = hi;
+     Comparable[] v = a[lo];
+     int i = lo;
+     while(i < gt){
+         int cmp = a[i].compareTo(v);
+         if(cmp < 0){
+             exch(a, lt++, i++);
+         }else if(i > gt){
+             exch(a, i, gt--);
+         }else{
+             i++;
+         }
+
+         sort(a, lo, lt - 1);
+         sort(a, gt + 1, hi);
+     }
+ }
+
+//System Sorts
+
+/** Java array sort 
+    $ Different method for each primitive type
+    $ Has method for data types that implements Comparable
+    $ Has method uses Comparator
+    $ Used tuned Quicksort for primitive types; Tuned mergesort for objects
+*/
+String[] a;
+Arrays.sort(a);
+/**
+    
 */
