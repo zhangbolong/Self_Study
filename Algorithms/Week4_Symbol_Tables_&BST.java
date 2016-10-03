9e64984ae0bf06ac46737ab74576f630804da38f
zhangbolong <boliu9441@gmail.com>
2016-10-02 20:50:25 -0400

    finish BST

--- Algorithms/Week4_Symbol_Tables_&BST.java
+++ Algorithms/Week4_Symbol_Tables_&BST.java
@@ -94,7 +94,122 @@ private int rank(Key key){
     return lo;
 }
 
+// Binary Search Trees
+/** Binary search Trees
+    $ Larger than all keys in its left subtree
+    $ Smaller than all keys in its right subtree
+
+    $ A node is compreised of four fields:
+            A key and a value.
+            A reference to the left and right subtree
+    $ Realistic Search time : 2*ln N
+    $ Realistic Tree height: 4.3 ln N
+    $ Worse case tree height: N
+*/
+
+/* Node represetation*/
+private class Node{
+    private Key key;
+    private Value val;
+    Private Node left, right;
+    public Node(Key key, Value value){
+        this.key = key;
+        this. val = val;
+    }
+}
 
 
+/* BST Implementation */
+public class BST<Key extends Comparable<Key>, Value>{
+    private node root;
+
+    private class Node{
+        private Key key;
+        private Value val;
+        Private Node left, right;
+        public Node(Key key, Value value){
+            this.key = key;
+            this. val = val;
+        }
+    }
+
+    public void put(Key key, Value val){
+        root = put(root, key, val);
+    }
+
+    // Private for put()
+    private Node put(Node x, Key key, Value val){
+        // Recursion stop statement
+        if(x == null) return new Node(key,val);
+        // Start recursion
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = put(x.left, key , val);
+        else if(cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        return x;
+    }
+
+    public Value get(Key key){
+        Node x = root;
+        While(x != null){
+            int cmp = key.compareTo(x.key);
+            if(cmp < 0){
+                x = x.left;
+            }else if(cmp > 0){
+                x = x.right;
+            }else{
+                return x.val;
+            }
+            return null;
+        }
+    }
+
+    public void deleteMin(){
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x){
+        if(x.left == null) return x. right;
+        x.left = deleteMin(x.left);
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key){
+        if(x == null) return null;
+        // Find the key want to delete
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = delete(x.left, key);
+        else if(cmp > 0) x.right = delete(x.right, key);
+        else{
+            if(x.right == null) return x.left;
+            if(x.left == null) return x.right;
+
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    } 
+
+    public Iterable<Key> iterator(){
+        Queue<key> q = new Queue<Key>();
+        inorder(root, q);
+        return q;
+    }
+
+    private void inorder(Node x, Queue<Key> q){
+        if(x ==null) return;
+        inorder(x.left, q);
+        q.enqueue(x.key);
+        inorder(x.right, q);
+    }
+}
 
 
